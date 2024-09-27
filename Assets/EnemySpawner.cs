f8b8cb07324d8656b3ad74c15bf0558c44636eb4
suji_sz <103244178+Yoonsuji@users.noreply.github.com>
2024-09-28 01:43:01 +0900

    feat:EnemySpawn

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,51 @@
+using Dreamteck.Splines;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject spawnPosition;
+    public GameObject[] enemy;
+    public SplineComputer enemySpline;
+    public float moveSpeed = 5f;
+
+    void Start()
+    {
+        StartCoroutine(SpawnEnemy());
+    }
+
+    IEnumerator SpawnEnemy()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(3f);
+            int index = Random.Range(0, enemy.Length);
+
+            GameObject spawnedEnemy = Instantiate(enemy[index], spawnPosition.transform.position, Quaternion.identity);
+
+            StartCoroutine(MoveAlongSpline(spawnedEnemy));
+        }
+    }
+
+    IEnumerator MoveAlongSpline(GameObject enemy)
+    {
+        int pointCount = enemySpline.pointCount;
+
+        for (int i = 0; i < pointCount - 1; i++)
+        {
+            Vector3 startPosition = enemySpline.GetPointPosition(i);
+            Vector3 endPosition = enemySpline.GetPointPosition(i + 1);
+
+            float t = 0f;
+
+            while (t < 1f)
+            {
+                t += Time.deltaTime * moveSpeed / Vector3.Distance(startPosition, endPosition);
+
+                enemy.transform.position = Vector3.Lerp(startPosition, endPosition, t);
+                yield return null;
+            }
+        }
+    }
+}
